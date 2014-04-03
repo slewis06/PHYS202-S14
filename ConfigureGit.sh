1292480315adbe22daa2d0a2d414ac312519f2ab
Sean Lewis <slewis06@calpoly.edu>
2014-04-03 15:01:36 -0700

    track Config script and pledge

---
+++ ConfigureGit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+git config --global user.name "Sean Lewis"
+git config --global user.email "slewis06@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
